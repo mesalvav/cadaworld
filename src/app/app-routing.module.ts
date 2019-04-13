5a54f3292572eb26adc5584a6b7165c9246aaac5
mario e salvatierra <mariohome@marios-mbp.attlocal.net>
2019-04-12 21:43:00 -0400

    added service and popular component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PopularComponent } from './components/popular/popular.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'populars', pathMatch: 'full' },
+  { path: 'populars', component: PopularComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
